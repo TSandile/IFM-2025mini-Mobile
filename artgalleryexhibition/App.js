107dcaa6c5131085d45ada3de68890f489e00668
TSandile <Sandile.Tshabalala11@gmail.com>
2025-10-06 23:17:13 +0200

    add updated files

--- artgalleryexhibition/App.js
+++ artgalleryexhibition/App.js
@@ -1,20 +1,133 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+import * as React from "react";
+//import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
+import ArtistScreen from "./src/pages/ArtistScreen";
+
+//b navigators
+const stackNavigator = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.screens}>
+      <Text>Home Screen </Text>
+      <Button title="Open Drawer" onPress={() => navigation.openDrawer()} />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={styles.screens}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+// stack navigator for home main page
+function HomeStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: { backgroundColor: "black" },
+        headerTintColor: "#fff", //text color of header
+        headerTitleStyle: { fontWeight: "bold" }, //text style of header
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          title: "  Art Gallery Exhibition", // title of header
+          headerLeft: () => (
+            <Ionicons
+              name="menu"
+              size={30}
+              color="#fff"
+              style={{ marginLeft: 15 }}
+              onPress={() => navigation.openDrawer()}
+            />
+          ),
+        })}
+      />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Screen name="Artist" component={ArtistScreen} />
+    </Stack.Navigator>
+  );
+}
+
+// drawer navigator for app
+function AppDrawer() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="HomeStack"
+      screenOptions={{
+        drawerActiveTintColor: "black", // coloer if item active
+        drawerInactiveTintColor: "#333", //coloer if item inactive
+        drawerLabelStyle: { fontSize: 16 }, //text style of item
+      }}
+    >
+      <Drawer.Screen
+        name="HomeStack"
+        component={HomeStack}
+        options={{
+          drawerLabel: "Home",
+          headerShown: false, // hide the header for drawer navigator
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Drawer.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{
+          drawerLabel: "Details",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Drawer.Screen
+        name="Artists"
+        component={ArtistScreen}
+        options={{
+          drawerLabel: "Artists",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AppDrawer />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  screens: { flex: 1, alignItems: "center", justifyContent: "center" },
 });
---
+++ artgalleryexhibition/src/pages/ArtistScreen.jsx
@@ -0,0 +1,12 @@
+import { styleSheet, Text, View, Button } from "react-native";
+import React from "react";
+
+const ArtistScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Artist Screen</Text>
+    </View>
+  );
+};
+
+export default ArtistScreen;
